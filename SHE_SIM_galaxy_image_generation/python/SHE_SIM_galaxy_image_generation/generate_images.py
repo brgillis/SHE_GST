b799f48f8428af3ce1090ceb230ff1540a9edf18
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-03-24 14:15:20 +0000

    Set up to try stable rng
    Former-commit-id: b7b57611337b76bac06cd72e746eff7e95554012

--- SHE_SIM_galaxy_image_generation/python/SHE_SIM_galaxy_image_generation/generate_images.py
+++ SHE_SIM_galaxy_image_generation/python/SHE_SIM_galaxy_image_generation/generate_images.py
@@ -43,6 +43,7 @@ from SHE_SIM_galaxy_image_generation.galaxy import (get_bulge_galaxy_profile,
                                              is_target_galaxy, get_disk_galaxy_profile,
                                              have_inclined_exponential)
 from SHE_SIM_galaxy_image_generation.magnitude_conversions import get_I
+from SHE_SIM_galaxy_image_generation.noise import get_sigma_ADU_per_pixel
 from SHE_SIM_galaxy_image_generation.psf import get_psf_profile
 from icebrgpy.logging import getLogger
 from icebrgpy.rebin import rebin
@@ -862,7 +863,8 @@ def generate_image(image, options):
 
     image_ID = image.get_full_ID()
 
-    sky_level_subtracted_pixel = image.get_param_value('subtracted_background') * pixel_scale ** 2
+    sky_level_subtracted = image.get_param_value('subtracted_background')
+    sky_level_subtracted_pixel = sky_level_subtracted * pixel_scale ** 2
     sky_level_unsubtracted_pixel = image.get_param_value('unsubtracted_background') * pixel_scale ** 2
 
     # For each dither
@@ -887,10 +889,17 @@ def generate_image(image, options):
 
         dither += sky_level_unsubtracted_pixel
         if not options['suppress_noise']:
-            noise = galsim.CCDNoise(galsim.BaseDeviate(image.get_full_seed() + 1),
-                                    gain=options['gain'],
-                                    read_noise=options['read_noise'],
-                                    sky_level=sky_level_subtracted_pixel)
+            if options['stable_rng']:
+                noise = get_sigma_ADU_per_pixel(pixel_value_ADU=dither.array,
+                            sky_level_ADU_per_sq_arcsec=sky_level_subtracted,
+                            read_noise_count=options['read_noise'],
+                            pixel_scale=pixel_scale,
+                            gain=options['gain'])*galsim.GaussianNoise(sigma=1.)
+            else:
+                noise = galsim.CCDNoise(galsim.BaseDeviate(image.get_full_seed() + 1),
+                                        gain=options['gain'],
+                                        read_noise=options['read_noise'],
+                                        sky_level=sky_level_subtracted_pixel)
             dither.addNoise(noise)
 
         # Add a header containing version info
@@ -923,7 +932,7 @@ def generate_image(image, options):
         logger.debug("Printing combined image.")
 
         # Get the base name for this combined image
-        combined_file_name_base = file_name_base + str(i) + "_combined"
+        combined_file_name_base = file_name_base + str(image_ID) + "_combined"
 
         # Get the table and (possibly changed) otable
         combined_image, combined_otable = combine_dithers(dithers=dithers,
---
+++ SHE_SIM_galaxy_image_generation/python/SHE_SIM_galaxy_image_generation/noise.py
@@ -0,0 +1,122 @@
+""" @file gain.py
+
+    Created 22 Mar 2017
+
+    Functions to handle needed conversions and calculations for noise
+    in simulated images.
+
+    ---------------------------------------------------------------------
+
+    Copyright (C) 2015 Bryan R. Gillis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from math import sqrt
+
+from SHE_SIM_galaxy_image_generation.gain import get_ADU_from_count, get_count_from_ADU
+
+def get_sky_level_ADU_per_pixel(sky_level_ADU_per_sq_arcsec,
+                                pixel_scale):
+    """ Calculate the sky level in units of ADU per pixel from the sky level per square arcsecond.
+
+        @param sky_level_ADU_per_sq_arcsec The sky level in units of ADU/arcsec^2
+        @param pixel_scale The pixel scale in units of arcsec/pixel
+
+        @return The sky level in units of ADU/pixel
+    """
+    
+    sky_level_ADU_per_pixel = sky_level_ADU_per_sq_arcsec * pixel_scale**2
+    
+    return sky_level_ADU_per_pixel
+
+def get_sky_level_count_per_pixel(sky_level_ADU_per_sq_arcsec,
+                                  pixel_scale,
+                                  gain):
+    """ Calculate the sky level in units of count per pixel from the sky level per square arcsecond.
+
+        @param sky_level_ADU_per_sq_arcsec The sky level in units of ADU/arcsec^2
+        @param pixel_scale The pixel scale in units of arcsec/pixel
+        @param gain The gain in units of e-/ADU
+
+        @return The sky level in units of e-/pixel
+    """
+    
+    sky_level_ADU_per_pixel = get_sky_level_ADU_per_pixel(sky_level_ADU_per_sq_arcsec,pixel_scale)
+    sky_level_count_per_pixel = get_count_from_ADU(sky_level_ADU_per_pixel,gain)
+    
+    return sky_level_count_per_pixel
+
+def get_count_lambda_per_pixel(pixel_value_ADU,
+                               sky_level_ADU_per_sq_arcsec,
+                               pixel_scale,
+                               gain):
+    """ Calculate the lambda of the Poisson distribution for a pixel's noise.
+
+        @param pixel_value The expected value of a pixel in ADU. Can be a scalar or array
+        @param sky_level_ADU_per_sq_arcsec The sky level in units of ADU/arcsec^2
+        @param pixel_scale The pixel scale in units of arcsec/pixel
+        @param gain The gain in units of e-/ADU
+
+        @return The lambda of the Poisson distribution in units of e-
+    """
+    
+    pixel_value_count = get_count_from_ADU(pixel_value_ADU, gain)
+    
+    sky_level_count_per_pixel = get_sky_level_count_per_pixel(sky_level_ADU_per_sq_arcsec,
+                                                              pixel_scale, gain)
+    
+    count_lambda = pixel_value_count + sky_level_count_per_pixel
+    
+    return count_lambda
+
+def get_read_noise_ADU_per_pixel(read_noise_count,
+                                 gain):
+    """ Calculate the read noise per pixel in units of ADU
+
+        @param read_noise_count The read noise in e-/pixel
+        @param gain The gain in units of e-/ADU
+
+        @return The read noise per pixel in units of ADU
+    """
+    
+    read_noise_ADU_per_pixel = get_ADU_from_count(read_noise_count, gain)
+    
+    return read_noise_ADU_per_pixel
+
+def get_sigma_ADU_per_pixel(pixel_value_ADU,
+                            sky_level_ADU_per_sq_arcsec,
+                            read_noise_count,
+                            pixel_scale,
+                            gain):
+    """ Calculate the sigma for Gaussian-like noise in units of ADU per pixel.
+
+        @param pixel_value The expected value of a pixel in ADU. Can be a scalar or array
+        @param sky_level_ADU_per_sq_arcsec The sky level in units of ADU/arcsec^2
+        @param read_noise_count The read noise in e-/pixel
+        @param pixel_scale The pixel scale in units of arcsec/pixel
+        @param gain The gain in units of e-/ADU
+
+        @return The sigma of the total noise in units of ADU per pixel
+    """
+    
+    count_lambda = get_count_lambda_per_pixel(pixel_value_ADU,
+                                              sky_level_ADU_per_sq_arcsec, pixel_scale, gain)
+    ADU_sigma_sq = get_ADU_from_count(get_ADU_from_count(count_lambda, gain)) # Apply twice since it's squared
+    
+    read_noise_ADU_per_pixel = get_read_noise_ADU_per_pixel(read_noise_count, gain)
+    
+    total_sigma = sqrt( ADU_sigma_sq + read_noise_ADU_per_pixel**2 )
+    
+    return total_sigma
