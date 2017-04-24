c0fb57679f45308fc1b49ee2a97945ccce0a2685
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-04-24 19:09:12 +0100

    correcting to runtime error
    Former-commit-id: 73aac0db01ce81c39ea4304eb787835ff7c7f9a9

--- SHE_GSM_ShearEstimation/python/SHE_GSM_ShearEstimation/estimate_shear.py
+++ SHE_GSM_ShearEstimation/python/SHE_GSM_ShearEstimation/estimate_shear.py
@@ -99,7 +99,7 @@ def estimate_shear_gs(galaxy_image, psf_image, gain, subtracted_sky_level,
             g2 = galsim_shear_estimate.corrected_g2
             mag = g1**2 + g2**2
             if mag > 1:
-                raise Exception("HSM Error: Magnitude of g shear is too large: " + str(mag))
+                raise RuntimeError("HSM Error: Magnitude of g shear is too large: " + str(mag))
         
             if np.abs(galsim_shear_estimate.corrected_shape_err) < 1e99:
                 shape_err = np.sqrt(shape_noise_var+galsim_shear_estimate.corrected_shape_err**2)
@@ -124,7 +124,7 @@ def estimate_shear_gs(galaxy_image, psf_image, gain, subtracted_sky_level,
                 
             shear_estimate = ShearEstimate(g1, g2, shape_err,)
         else:
-            raise Exception("Invalid shear estimation method for GalSim: " + str(method))
+            raise RuntimeError("Invalid shear estimation method for GalSim: " + str(method))
             
             
     except RuntimeError as e:
