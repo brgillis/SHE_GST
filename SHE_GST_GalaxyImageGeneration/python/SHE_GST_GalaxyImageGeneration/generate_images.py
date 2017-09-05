c5dbd1a8db36e4e758e64c22977101f215c2b70b
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-09-05 18:37:07 +0100

    Adding logging
    Former-commit-id: a1ac17fc19edf3737040025b93b5eb565b36ec6c

--- SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/generate_images.py
+++ SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/generate_images.py
@@ -984,6 +984,7 @@ def generate_image(image, options):
         # Output the datafile if necessary
 
         if not options['details_output_format']=='none':
+            logger.info("Printing tables for dither " + str(di) + ".")
             # Temporarily adjust centre positions by dithering
             details_table[datf.gal_x] += x_offset
             details_table[datf.gal_y] += y_offset
@@ -999,6 +1000,7 @@ def generate_image(image, options):
             # Undo dithering adjustment
             details_table[datf.gal_x] -= x_offset
             details_table[datf.gal_y] -= y_offset
+            logger.info("Finished printing tables for dither " + str(di) + ".")
 
         logger.info("Finished printing dither " + str(di) + ".")
 
