526234326f93894f08042ce0c2836dfca94e70b2
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-09-08 16:13:09 +0100

    Fixing autofilling
    Former-commit-id: 4538303b5ff38362575611342f8aec6f5bd76562

--- SHE_GST_PhysicalModel/src/lib/levels/ImageGroup.cpp
+++ SHE_GST_PhysicalModel/src/lib/levels/ImageGroup.cpp
@@ -6,17 +6,17 @@
 
  **********************************************************************
 
- Copyright (C) 2012-2020 Euclid Science Ground Segment      
+ Copyright (C) 2012-2020 Euclid Science Ground Segment
 
- This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General    
- Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)    
- any later version.    
+ This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+ any later version.
 
- This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied    
- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more    
- details.    
+ This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ details.
 
- You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to    
+ You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
  \**********************************************************************/
@@ -51,6 +51,16 @@ Image * ImageGroup::add_image()
 void ImageGroup::add_images(int_t const & N)
 {
 	return ParamHierarchyLevel::spawn_children<Image>(N);
+}
+
+#endif
+
+// Methods to automatically add children
+#if(1)
+
+void ImageGroup::fill_images()
+{
+     add_images( round_int(get_param_value(num_images_name)) );
 }
 
 #endif
