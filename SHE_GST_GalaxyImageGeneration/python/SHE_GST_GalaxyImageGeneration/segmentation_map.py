caa09f594e5c88faaf75799299113ca53027a032
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2017-10-05 17:46:13 +0100

    Adding first pass at function to create a mock segmentation map
    Former-commit-id: 81eb1f1f865049a7cf5005aa3c42a28465399c35

---
+++ SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/segmentation_map.py
@@ -0,0 +1,68 @@
+""" @file segmentation_map.py
+
+    Created 5 Oct 2017
+
+    Functions to generate mock segmentation maps.
+
+    ---------------------------------------------------------------------
+
+    Copyright (C) 2012-2020 Euclid Science Ground Segment      
+       
+    This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General    
+    Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)    
+    any later version.    
+       
+    This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied    
+    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more    
+    details.    
+       
+    You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to    
+    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+"""
+
+import numpy as np
+import galsim
+
+from SHE_PPT.detections_table_format import tf as detf
+
+def make_segmentation_map( noisefree_image,
+                           detections_table,
+                           threshold=0,
+                           r_max=256 ):
+    """
+        @TODO Docstring
+    """
+    
+    segmentation_map = galsim.Image(np.ones_like(noisefree_image,dtype=np.int32),scale=noisefree_image.scale)
+    
+    y_image, x_image = np.indices(np.shape(noisefree_image.array))
+    
+    r2_max = r_max**2
+    
+    threshold_mask = np.ravel(noisefree_image.array) <= threshold
+    claimed_mask = np.zeros_like(threshold_mask,dtype=bool)
+    
+    for i in range(len(detections_table)):
+        
+        # For each object, look for pixels near it above the threshold value
+        dx_image = x_image - detections_table[detf.gal_x][i]
+        dy_image = y_image - detections_table[detf.gal_x][i]
+        
+        r2_image = dx_image**2 + dy_image**2
+        
+        region_mask = np.ravel(r2_image) > r2_max
+        
+        claimed_threshold_mask = np.logical_or(threshold_mask,claimed_mask)
+        
+        full_mask = np.logical_or(region_mask,claimed_threshold_mask)
+        
+        # Set the unmasked values to the object's ID
+        np.ma.masked_array(np.ravel(segmentation_map.array),full_mask)[~full_mask] *= detections_table[detf.ID][i]
+        
+        # Add those values to the claimed mask
+        claimed_mask = np.logical_or(claimed_mask,~full_mask)
+        
+    # Set all unclaimed values to -1
+    np.ma.masked_array(np.ravel(segmentation_map.array),claimed_mask)[~claimed_mask] *= -1
+    
+    return segmentation_map
