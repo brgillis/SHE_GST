f5cdaef5e1228640ea75b5d7a96a5cf03bf443e6
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-06-12 12:03:16 +0200

    Adding basic test of WCS
    Former-commit-id: 891eb83ccf580a08a2aa3db1de5b05fe1ecea412

---
+++ SHE_GST_GalaxyImageGeneration/tests/python/wcs_test.py
@@ -0,0 +1,45 @@
+""" @file gain_test.py
+
+    Created 12 June 2018
+
+    Tests of functions dealing with creating a GalSim WCS.
+"""
+
+# Copyright (C) 2012-2020 Euclid Science Ground Segment
+#
+# This library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General
+# Public License as published by the Free Software Foundation; either version 3.0 of the License, or (at your option)
+# any later version.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+import pytest
+
+from numpy.testing import assert_almost_equal
+
+class TestWCS:
+    """
+
+
+    """
+    
+    @classmethod
+    def setup_class(cls):
+
+        return
+
+    @classmethod
+    def teardown_class(cls):
+
+        return
+    
+    def test_get_wcs(self):
+        
+        return
+    
+    
