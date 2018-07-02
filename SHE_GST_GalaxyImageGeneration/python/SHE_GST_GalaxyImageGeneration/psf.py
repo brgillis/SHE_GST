0943c4590ac1febf0fa0245a51e3f541ea75de7f
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-07-02 15:21:33 +0100

    Find model psf file in path
    Former-commit-id: ce09fef69be01ff17e15a0ea5e933bce9180839e

--- SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/psf.py
+++ SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/psf.py
@@ -18,37 +18,38 @@
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
+from copy import deepcopy
+from functools import lru_cache
 from os.path import join
 
 from astropy.io import fits
 from astropy.table import Table
 import galsim
+
 import SHE_GST_GalaxyImageGeneration.magic_values as mv
-from functools import lru_cache
-import numpy as np
 from SHE_PPT.file_io import find_file, append_hdu
-from SHE_PPT.table_utility import table_to_hdu
-from SHE_PPT.table_formats.psf import tf as pstf
-from copy import deepcopy
 from SHE_PPT.magic_values import bulge_psf_tag, disk_psf_tag
+from SHE_PPT.table_formats.psf import tf as pstf
+from SHE_PPT.table_utility import table_to_hdu
 from coverage.html import data_filename
+import numpy as np
+
 
 # Magic values for this module
-
-sed_names = {'ell':'el_cb2004a_001',
-             'sbc':'sbc_cb2004a_001',
-             'scd':'scd_cb2004a_001',
-             'sb2':'sb2_b2004a_001',
-             'sb3':'sb3_b2004a_001',
+sed_names = {'ell': 'el_cb2004a_001',
+             'sbc': 'sbc_cb2004a_001',
+             'scd': 'scd_cb2004a_001',
+             'sb2': 'sb2_b2004a_001',
+             'sb3': 'sb3_b2004a_001',
              }
 
-seds = {4.0:'sbc',
-        3.5:'sbc',
-        3.0:'sbc',
-        2.71:'scd',
-        2.56:'sb2',
-        2.0:'sb3',
-        1.8:'sb3',
+seds = {4.0: 'sbc',
+        3.5: 'sbc',
+        3.0: 'sbc',
+        2.71: 'scd',
+        2.56: 'sb2',
+        2.0: 'sb3',
+        1.8: 'sb3',
         }
 
 allowed_ns = np.array((1.8, 2.0, 2.56, 2.71, 3.0, 3.5, 4.0))
@@ -59,12 +60,13 @@ exposure_index_label = "EX_INDEX"
 scale_label = "GS_SCALE"
 type_label = "PSF_TYPE"
 
+
 @lru_cache()
 def load_psf_model_from_sed_z(sed,
-                               z = 0.0,
-                               data_dir = mv.default_data_dir,
-                               gsparams = galsim.GSParams(),
-                               workdir = "."):
+                              z=0.0,
+                              data_dir=mv.default_data_dir,
+                              gsparams=galsim.GSParams(),
+                              workdir="."):
 
     z_str = "%0.2f" % z
 
@@ -73,52 +75,56 @@ def load_psf_model_from_sed_z(sed,
     qualified_filename = find_file(model_filename, workdir)
 
     return load_psf_model_from_file(qualified_filename,
-                                     scale = mv.psf_model_scale,
-                                     offset = mv.default_psf_center_offset,
-                                     gsparams = gsparams)
+                                    scale=mv.psf_model_scale,
+                                    offset=mv.default_psf_center_offset,
+                                    gsparams=gsparams)
+
 
 @lru_cache()
 def load_psf_model_from_file(file_name,
-                              scale,
-                              offset,
-                              gsparams = galsim.GSParams()):
+                             scale,
+                             offset,
+                             gsparams=galsim.GSParams()):
 
     model = galsim.fits.read(file_name)
 
     return galsim.InterpolatedImage(model,
-                                     scale = scale,
-                                     offset = offset,
-                                     gsparams = gsparams)
+                                    scale=scale,
+                                    offset=offset,
+                                    gsparams=gsparams)
+
 
 @lru_cache()
-def get_background_psf_profile(gsparams = galsim.GSParams()):
+def get_background_psf_profile(gsparams=galsim.GSParams()):
 
-    prof = galsim.OpticalPSF(lam = 725,  # nm
-                             diam = 1.2,  # m
-                             defocus = 0,
-                             obscuration = 0.33,
-                             nstruts = 3,
-                             gsparams = gsparams,
+    prof = galsim.OpticalPSF(lam=725,  # nm
+                             diam=1.2,  # m
+                             defocus=0,
+                             obscuration=0.33,
+                             nstruts=3,
+                             gsparams=gsparams,
                              )
 
     return prof
 
+
 def get_psf_profile(n,
-                     z,
-                     bulge,
-                     use_background_psf = False,
-                     data_dir = mv.default_data_dir,
-                     model_psf_file_name = None,
-                     model_psf_scale = mv.psf_model_scale,
-                     model_psf_offset = mv.default_psf_center_offset,
-                     gsparams = galsim.GSParams(),
-                     workdir = "."):
+                    z,
+                    bulge,
+                    use_background_psf=False,
+                    data_dir=mv.default_data_dir,
+                    model_psf_file_name=None,
+                    model_psf_scale=mv.psf_model_scale,
+                    model_psf_offset=mv.default_psf_center_offset,
+                    gsparams=galsim.GSParams(),
+                    workdir="."):
 
     if use_background_psf:
-        return get_background_psf_profile(gsparams = gsparams)
+        return get_background_psf_profile(gsparams=gsparams)
 
     if model_psf_file_name is not None:
-        return load_psf_model_from_file(model_psf_file_name, model_psf_scale, model_psf_offset, gsparams = gsparams)
+        qualified_model_filename = find_file(model_psf_file_name, workdir)
+        return load_psf_model_from_file(qualified_model_filename, model_psf_scale, model_psf_offset, gsparams=gsparams)
 
     diffs = np.abs(allowed_zs - z)
     zi_best = np.argmin(diffs)
@@ -130,23 +136,24 @@ def get_psf_profile(n,
         ni_best = np.argmin(diffs)
         sed = seds[allowed_ns[ni_best]]
 
-    return load_psf_model_from_sed_z(sed, allowed_zs[zi_best], gsparams = gsparams, data_dir = data_dir)
+    return load_psf_model_from_sed_z(sed, allowed_zs[zi_best], gsparams=gsparams, data_dir=data_dir)
+
 
 def create_psf_hdu(psf_profile,
                    galaxy_id,
                    exposure_index,
-                   stamp_size = mv.default_psf_stamp_size,
-                   scale = mv.default_pixel_scale / mv.default_psf_scale_factor,
-                   psf_type = "bulge"):
+                   stamp_size=mv.default_psf_stamp_size,
+                   scale=mv.default_pixel_scale / mv.default_psf_scale_factor,
+                   psf_type="bulge"):
     """Creates an HDU of an image of a PSF profile.
     """
 
     # Draw the profile onto an image of the proper size
-    psf_image = galsim.ImageF(stamp_size, stamp_size, scale = scale)
+    psf_image = galsim.ImageF(stamp_size, stamp_size, scale=scale)
     psf_profile.drawImage(psf_image)
 
     # Set up an image HDU with this image
-    psf_hdu = fits.ImageHDU(data = psf_image.array)
+    psf_hdu = fits.ImageHDU(data=psf_image.array)
 
     # Add needed keywords to the header of this HDU
     psf_hdu.header[gal_id_label] = galaxy_id
@@ -157,24 +164,25 @@ def create_psf_hdu(psf_profile,
     # Return
     return psf_hdu
 
+
 def add_psf_to_archive(psf_profile,
                        archive_filename,
                        galaxy_id,
                        exposure_index,
                        psf_type,
-                       stamp_size = mv.default_psf_stamp_size,
-                       scale = mv.default_pixel_scale / mv.default_psf_scale_factor,
-                       workdir = "."):
+                       stamp_size=mv.default_psf_stamp_size,
+                       scale=mv.default_pixel_scale / mv.default_psf_scale_factor,
+                       workdir="."):
     """Creates a PSF HDU and saves it to an archive file.
     """
 
     # Create the HDU
-    psf_hdu = create_psf_hdu(psf_profile = psf_profile,
-                             galaxy_id = galaxy_id,
-                             exposure_index = exposure_index,
-                             stamp_size = stamp_size,
-                             scale = scale,
-                             psf_type = psf_type)
+    psf_hdu = create_psf_hdu(psf_profile=psf_profile,
+                             galaxy_id=galaxy_id,
+                             exposure_index=exposure_index,
+                             stamp_size=stamp_size,
+                             scale=scale,
+                             psf_type=psf_type)
 
     # Append the HDU to the archive
 
@@ -183,6 +191,7 @@ def add_psf_to_archive(psf_profile,
 
     return
 
+
 def get_psf_from_archive(archive_hdulist,
                          galaxy_id,
                          exposure_index):
@@ -190,18 +199,19 @@ def get_psf_from_archive(archive_hdulist,
     for hdu in archive_hdulist:
         if hdu.header[gal_id_label] == galaxy_id and hdu.header[exposure_index_label] == exposure_index:
 
-            psf_image = galsim.ImageF(hdu.data, scale = hdu.header[scale_label])
+            psf_image = galsim.ImageF(hdu.data, scale=hdu.header[scale_label])
 
             return psf_image
 
     raise ValueError("PSF for galaxy " + str(galaxy_id) + " for exposure " + str(exposure_index) +
                      " not found in PSF archive.")
 
+
 def sort_psfs_from_archive(psf_table,
                            psf_data_filename,
                            psf_archive_filename,
                            exposure_index,
-                           workdir = "."):
+                           workdir="."):
 
     qualified_psf_data_filename = join(workdir, psf_data_filename)
 
@@ -212,7 +222,7 @@ def sort_psfs_from_archive(psf_table,
     psf_table.add_index(pstf.ID)  # Allow it to be indexed by galaxy ID
 
     # Open the archive to work with
-    archive_hdulist = fits.open(join(workdir, psf_archive_filename), mode = 'denywrite', memmap = True)
+    archive_hdulist = fits.open(join(workdir, psf_archive_filename), mode='denywrite', memmap=True)
 
     hdu_index = 2  # Start indexing at 2, since 0 is empty and 1 is table
     for hdu in archive_hdulist:
@@ -233,8 +243,8 @@ def sort_psfs_from_archive(psf_table,
             header['EXTNAME'] = str(gal_id) + "." + disk_psf_tag
 
         # Add to the data file
-        psf_hdu = fits.ImageHDU(data = hdu.data,
-                                header = header)
+        psf_hdu = fits.ImageHDU(data=hdu.data,
+                                header=header)
         append_hdu(qualified_psf_data_filename, psf_hdu)
 
         # Update the PSF table with the HDU index of this
@@ -246,7 +256,7 @@ def sort_psfs_from_archive(psf_table,
 
     # Now that the table is complete, update the values for it in the fits file
 
-    f = fits.open(qualified_psf_data_filename, memmap = True, mode = 'update')
+    f = fits.open(qualified_psf_data_filename, memmap=True, mode='update')
     out_table = f[1].data
 
     out_table[pstf.bulge_index] = psf_table[pstf.bulge_index]
@@ -256,10 +266,3 @@ def sort_psfs_from_archive(psf_table,
     f.close()
 
     return
-
-
-
-
-
-
-
