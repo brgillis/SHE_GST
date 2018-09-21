3282b15c8aeae9a9b8a6436e5a1bd2a6cbe42a1c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2018-09-21 16:32:03 +0100

    Adding test of zero steps
    Former-commit-id: c4cdb5cbc092d43a308edbc98097fedde7a01b10

--- SHE_GST_PrepareConfigs/tests/python/write_configs_test.py
+++ SHE_GST_PrepareConfigs/tests/python/write_configs_test.py
@@ -5,7 +5,7 @@
     Contains unit tests of functions in SHE_GST_PrepareConfigs/write_configs.py
 """
 
-__updated__ = "2018-08-07"
+__updated__ = "2018-09-21"
 
 # Copyright (C) 2012-2020 Euclid Science Ground Segment
 #
@@ -21,12 +21,13 @@ __updated__ = "2018-08-07"
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 import os
-
 import pytest
+
+from SHE_PPT.file_io import (find_file, read_listfile, get_data_filename)
 
 from SHE_GST_GalaxyImageGeneration.config.parse_config import get_cfg_args
 from SHE_GST_PrepareConfigs.write_configs import write_config, write_configs_from_plan
-from SHE_PPT.file_io import (find_file, read_listfile, get_data_filename)
+from astropy.table import Table
 
 
 class TestWriteConfigs:
@@ -45,13 +46,14 @@ class TestWriteConfigs:
 
         return
 
-    @pytest.fixture(autouse = True)
+    @pytest.fixture(autouse=True)
     def setup(self, tmpdir):
         self.workdir = tmpdir.strpath
-        os.mkdir(os.path.join(self.workdir,"data"))
-
+        os.mkdir(os.path.join(self.workdir, "data"))
 
     def test_write_config(self):
+        """ General tests of the write_config function.
+        """
 
         filename = os.path.join(self.workdir, "test_config.junk")
         template_filename = find_file("AUX/SHE_GST_PrepareConfigs/StampsTemplate.conf")
@@ -64,16 +66,16 @@ class TestWriteConfigs:
         render_background = True
 
         write_config(filename,
-                      template_filename,
-                      model_seed = model_seed,
-                      noise_seed = noise_seed,
-                      suppress_noise = suppress_noise,
-                      num_detectors = num_detectors,
-                      num_galaxies = num_galaxies,
-                      render_background = render_background)
+                     template_filename,
+                     model_seed=model_seed,
+                     noise_seed=noise_seed,
+                     suppress_noise=suppress_noise,
+                     num_detectors=num_detectors,
+                     num_galaxies=num_galaxies,
+                     render_background=render_background)
 
         # Load the config and see if it matches our expectations
-        cfg_args = get_cfg_args(filename, workdir = self.workdir)
+        cfg_args = get_cfg_args(filename, workdir=self.workdir)
 
         assert cfg_args["seed"] == model_seed
         assert cfg_args["noise_seed"] == noise_seed
@@ -85,6 +87,8 @@ class TestWriteConfigs:
         return
 
     def test_write_configs_from_plan(self):
+        """ General tests of the write_configs_from_plan function.
+        """
 
         listfile_filename = os.path.join(self.workdir, "test_listfile.junk")
         template_filename = find_file("AUX/SHE_GST_PrepareConfigs/StampsTemplate.conf")
@@ -93,7 +97,7 @@ class TestWriteConfigs:
         write_configs_from_plan(plan_filename,
                                 template_filename,
                                 listfile_filename,
-                                workdir = self.workdir)
+                                workdir=self.workdir)
 
         config_filenames = read_listfile(listfile_filename)
 
@@ -103,9 +107,9 @@ class TestWriteConfigs:
 
             # Check first set of 10
             config_p_filename = config_filenames[i]
-            config_filename = get_data_filename(config_p_filename, workdir = self.workdir)
+            config_filename = get_data_filename(config_p_filename, workdir=self.workdir)
 
-            cfg_args = get_cfg_args(config_filename, workdir = self.workdir)
+            cfg_args = get_cfg_args(config_filename, workdir=self.workdir)
 
             assert cfg_args["seed"] == i + 1
             assert cfg_args["noise_seed"] == i + 1
@@ -116,9 +120,9 @@ class TestWriteConfigs:
 
             # Check second set of 10
             config_p_filename = config_filenames[i + 10]
-            config_filename = get_data_filename(config_p_filename, workdir = self.workdir)
+            config_filename = get_data_filename(config_p_filename, workdir=self.workdir)
 
-            cfg_args = get_cfg_args(config_filename, workdir = self.workdir)
+            cfg_args = get_cfg_args(config_filename, workdir=self.workdir)
 
             assert cfg_args["seed"] == i + 1
             assert cfg_args["noise_seed"] == i + 1
@@ -129,3 +133,116 @@ class TestWriteConfigs:
 
         return
 
+    def test_zero_steps(self):
+        """ Tests of write_configs_from_plan that test zero step size for model/noise seed.
+        """
+
+        listfile_filename = os.path.join(self.workdir, "test_listfile.junk")
+        template_filename = find_file("AUX/SHE_GST_PrepareConfigs/StampsTemplate.conf")
+        base_plan_filename = find_file("AUX/SHE_GST_PrepareConfigs/test_simulation_plan.fits")
+
+        num_seeds = 5
+
+        # Test for zero model seed step
+
+        base_plan = Table.read(base_plan_filename, format="fits")
+        base_plan.remove_row(1)  # Remove the second row for these tests
+        base_plan[0]["MSEED_MIN"] = 15
+        base_plan[0]["MSEED_MAX"] = 15
+        base_plan[0]["MSEED_STEP"] = 0
+        base_plan[0]["NSEED_MIN"] = 2
+        base_plan[0]["NSEED_MAX"] = num_seeds * 2
+        base_plan[0]["NSEED_STEP"] = 2
+
+        write_configs_from_plan(plan_filename,
+                                template_filename,
+                                listfile_filename,
+                                workdir=self.workdir)
+
+        config_filenames = read_listfile(listfile_filename)
+
+        assert len(config_filenames) == num_seeds
+
+        for i in range(num_seeds):
+
+            config_p_filename = config_filenames[i]
+            config_filename = get_data_filename(config_p_filename, workdir=self.workdir)
+
+            cfg_args = get_cfg_args(config_filename, workdir=self.workdir)
+
+            assert cfg_args["seed"] == 15
+            assert cfg_args["noise_seed"] == 2 * i + 2
+            assert cfg_args["suppress_noise"] == False
+            assert cfg_args["num_images"] == 36
+            assert cfg_args["num_target_galaxies"] == 1024
+            assert cfg_args["render_background_galaxies"] == True
+
+        # Test for zero noise seed step
+
+        base_plan = Table.read(base_plan_filename, format="fits")
+        base_plan.remove_row(1)  # Remove the second row for these tests
+        base_plan[0]["MSEED_MIN"] = 4
+        base_plan[0]["MSEED_MAX"] = 2 + num_seeds * 2
+        base_plan[0]["MSEED_STEP"] = 2
+        base_plan[0]["NSEED_MIN"] = 3
+        base_plan[0]["NSEED_MAX"] = 3
+        base_plan[0]["NSEED_STEP"] = 0
+
+        write_configs_from_plan(plan_filename,
+                                template_filename,
+                                listfile_filename,
+                                workdir=self.workdir)
+
+        config_filenames = read_listfile(listfile_filename)
+
+        assert len(config_filenames) == num_seeds
+
+        for i in range(num_seeds):
+
+            config_p_filename = config_filenames[i]
+            config_filename = get_data_filename(config_p_filename, workdir=self.workdir)
+
+            cfg_args = get_cfg_args(config_filename, workdir=self.workdir)
+
+            assert cfg_args["seed"] == 2 * i + 4
+            assert cfg_args["noise_seed"] == 3
+            assert cfg_args["suppress_noise"] == False
+            assert cfg_args["num_images"] == 36
+            assert cfg_args["num_target_galaxies"] == 1024
+            assert cfg_args["render_background_galaxies"] == True
+
+        # Test for zero model and noise seed steps
+
+        base_plan = Table.read(base_plan_filename, format="fits")
+        base_plan.remove_row(1)  # Remove the second row for these tests
+        base_plan[0]["MSEED_MIN"] = 15
+        base_plan[0]["MSEED_MAX"] = 15
+        base_plan[0]["MSEED_STEP"] = 0
+        base_plan[0]["NSEED_MIN"] = 3
+        base_plan[0]["NSEED_MAX"] = 3
+        base_plan[0]["NSEED_STEP"] = 0
+
+        write_configs_from_plan(plan_filename,
+                                template_filename,
+                                listfile_filename,
+                                workdir=self.workdir)
+
+        config_filenames = read_listfile(listfile_filename)
+
+        assert len(config_filenames) == 1
+
+        for i in range(num_seeds):
+
+            config_p_filename = config_filenames[i]
+            config_filename = get_data_filename(config_p_filename, workdir=self.workdir)
+
+            cfg_args = get_cfg_args(config_filename, workdir=self.workdir)
+
+            assert cfg_args["seed"] == 15
+            assert cfg_args["noise_seed"] == 3
+            assert cfg_args["suppress_noise"] == False
+            assert cfg_args["num_images"] == 36
+            assert cfg_args["num_target_galaxies"] == 1024
+            assert cfg_args["render_background_galaxies"] == True
+
+        return
