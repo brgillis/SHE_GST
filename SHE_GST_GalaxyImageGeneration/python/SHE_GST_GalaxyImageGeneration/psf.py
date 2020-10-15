ffcc7070026af6df0de601c07135c2ad4e7d4cde
David Copeland <dcopelan@ed.ac.uk>
2020-10-15 14:31:13 +0100

    Updated names to reflect SHE_PPT table formats changes
    Former-commit-id: 4faf1bd7651957540e073bb79fc97464eecbe6f2

--- SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/psf.py
+++ SHE_GST_GalaxyImageGeneration/python/SHE_GST_GalaxyImageGeneration/psf.py
@@ -31,7 +31,7 @@ import SHE_GST_GalaxyImageGeneration.magic_values as mv
 from SHE_PPT.file_io import find_file
 from SHE_PPT.logging import getLogger
 from SHE_PPT.magic_values import bulge_psf_tag, disk_psf_tag
-from SHE_PPT.table_formats.psf import tf as pstf
+from SHE_PPT.table_formats.she_psf_model_image import tf as pstf
 from SHE_PPT.table_utility import table_to_hdu
 import numpy as np
 
