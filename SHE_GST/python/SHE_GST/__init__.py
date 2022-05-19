2d3725e94d5a7bae9ac2a903476af51dc3a5de4c
Bryan Gillis <7204836+brgillis@users.noreply.github.com>
2022-05-19 16:45:14 +0100

    Getting version number programmatically
    Former-commit-id: e637bdd9d44a628da6ea2c8222f795dc4c0f4636

--- SHE_GST/python/SHE_GST/__init__.py
+++ SHE_GST/python/SHE_GST/__init__.py
@@ -20,4 +20,7 @@ __updated__ = "2021-08-19"
 # You should have received a copy of the GNU Lesser General Public License along with this library; if not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-__version__ = "8.3"
+# Get the version from the compiled file created by Elements
+from SHE_GST_VERSION import SHE_GST_VERSION_STRING
+
+__version__ = SHE_GST_VERSION_STRING
